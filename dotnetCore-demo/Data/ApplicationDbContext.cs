bf3fb706904297888f8aa621eadea30e90ac7503
Nguyen Hoang Viet <hoangviett09@gmail.com>
2023-10-16 16:24:33 +0700

    first commit

---
+++ dotnetCore-demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using dotnetCore_demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetCore_demo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
